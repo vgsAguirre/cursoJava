e6f08094a7c896dfb5d3799737f718230447b162
vgsAguirre <vgs.aguirre@gmail.com>
2020-05-31 15:12:14 -0300

    first commit

---
+++ src/test/java/com/educandoweb/cursojava/CursojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cursojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
